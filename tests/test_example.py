0f143c1e3f73af73aecf2cc627ce6d6aab8dfed7
Adam J. Jackson <a.j.jackson@physics.org>
2026-02-25 15:51:48 +0000

    Create minimal pytest and Github Actions workflow

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy.testing import assert_allclose
+
+def test_module_imports():
+    import castep_adp
+
+def test_bad_array_compare():
+    a = np.array([0, 1, 2])
+    b = np.array([0, 1, 4])
+
+    assert_allclose(a, b)
