34df596426e148d29b4f6556246af3b1305fb4b7
Kit Brown <kit.brown@york.ac.uk>
2026-05-07 10:28:25 +0100

    basic overall tests

---
+++ tests/data/test_N2_2.adp
@@ -0,0 +1,8 @@
+calculate_adp : false           
+calculate_ke : true             
+equilibration_timesteps : 0
+r_equilibrium : finite          
+write_adp : false               
+write_jmol : false              
+write_ke : true                               
+write_uij : false               
---
+++ tests/test_overall.py
@@ -0,0 +1,22 @@
+from castep_adp import parse, settings, adp_io, obj, cli
+
+def test_module_imports():
+  import castep_adp
+
+def test_run():
+  seed = "tests/data/test_N2_2"
+  user_settings = settings.Settings(seed)
+  md = parse.parse_md(seed,user_settings.settings["equilibration_timesteps"])
+  atoms = {}
+  for label in md.atoms:
+    atoms[label] = obj.Atom(label)
+  keys = list(atoms.keys())
+
+  r_eq = cli.calc_r_eq_from_md(md)
+  ke = cli.calc_ke_tensor(md)
+
+  for i in range(len(keys)):
+    atoms[keys[i]].r_eq = r_eq[i]
+    atoms[keys[i]].ke = ke[i]
+
+  adp_io.write_out(seed,user_settings,atoms,False)
